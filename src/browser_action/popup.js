3e1d2186e97c0c5b4e2e32c71aef9fc38eb8be1d
Hendra Susanto <benhanks040888@gmail.com>
2017-11-17 16:42:22 +0700

    update icons and small change in code

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,13 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
   var twitterSetAllVerified;
+
   chrome.storage.sync.get("twitterSetAllVerified", function(result){
     twitterSetAllVerified = result.twitterSetAllVerified;
-    // set js-status accordingly
+
     var status = document.querySelector('.js-status');
     var buttonLabel = document.querySelector('.js-toggle');
     
-    buttonLabel.innerHTML = twitterSetAllVerified ? 'Unverify \'em all!' : 'Verify \'em all!';
-    status.innerHTML = twitterSetAllVerified ? 'Everyone is verified' : 'Everyone is not verified';
+    buttonLabel.innerHTML = twitterSetAllVerified ? 'Unverify everyone!' : 'Verify everyone!';
+    if (twitterSetAllVerified === true) {
+      status.innerHTML = 'Everyone is verified';
+    } else if (twitterSetAllVerified === false) {
+      status.innerHTML = 'Everyone is not verified';
+    } else {
+      status.innerHTML = 'Twitter default';
+    }
   });
 
   document.querySelector('.js-toggle').addEventListener('click', function() {
@@ -19,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  document.querySelector('.js-reset').addEventListener('click', function() {
+  document.querySelector('.js-reset').addEventListener('click', function(e) {
     chrome.storage.sync.clear(function() {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.update(tabs[0].id, {url: tabs[0].url});
